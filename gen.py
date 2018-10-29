df1cb717e78b2833df1b6647d69e9d451593277a
sysadmin2 <vladbuk@gmail.com>
2018-10-29 19:42:33 +0200

    try-except default arguments added

--- gen.py
+++ gen.py
@@ -2,16 +2,20 @@ from genpass_class import Genpass
 from sys import argv
 
 def main(argv):
-    length = int(argv[1])
-    power = int(argv[2])
+    try:
+        length = int(argv[1])
+        power = int(argv[2])
+    except IndexError:
+        # if no arguments in command line added
+        print('Default arguments used')
+        length = 12
+        power = 2
 
     pass1 = Genpass(length, power)
 
     for _ in range(10):
         print(pass1)
 
-# main(argv)
-
 if __name__ == "__main__":
    main(argv)
-#    main(argv[1:])
+   input('Press "Enter" to exit')
